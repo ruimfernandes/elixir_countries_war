1bfa48cda56901dbe95e5947b63ca36022832ade
Rui Fernandes <rui.fernandes@utrust.com>
2021-05-17 16:10:52 +0100

    Refactor country supervisor from deprecated functions to the new functions

--- lib/elixir_countries_war/country/supervisor.ex
+++ lib/elixir_countries_war/country/supervisor.ex
@@ -6,17 +6,12 @@ defmodule ElixirCountriesWar.Country.Supervisor do
     Supervisor.start_link(__MODULE__, :ok)
   end
 
+  @impl true
   def init(:ok) do
     children = [
-      worker(Country, [%{name: "Portugal", citizens: 0}])
+      {Country, %{name: "Portugal", citizens: 0}}
     ]
 
-    # {:ok, pid} = supervise(children, strategy: :one_for_one)
-
-    # {:ok, pid} = Supervisor.start_link(children, strategy: :one_for_one)
-    # IO.inspect "I'm a supervisor"
-    # IO.inspect pid
-
-    supervise(children, strategy: :one_for_one)
+    Supervisor.init(children, strategy: :one_for_one)
   end
 end
